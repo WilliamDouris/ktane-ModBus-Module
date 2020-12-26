5ef57334c2aef86a24b03bbb1984441328fd502e
WilliamDouris <william.douris@me.com>
2020-12-26 23:39:22 +0100

    Change most of the code + Correct some things on manual
    Script, need to be done :
    - Handle press button

--- Assets/ModBusScript.cs
+++ Assets/ModBusScript.cs
@@ -7,7 +7,8 @@ using Newtonsoft.Json;
 using KMHelper;
 using System;
 
-public class ModBusScript : MonoBehaviour {
+public class ModBusScript : MonoBehaviour
+{
 
     public class ModSettingsJSON
     {
@@ -29,60 +30,224 @@ public class ModBusScript : MonoBehaviour {
     //_isSolved = true; at the end of module
     //_lightsOn = true; in Activate()
 
-    private int digit = 0; //Digit currently writing
-    private char[] CorrectAns = new char[17]; //Answer correct.
-    private char[] YourAns = new char[17]; //Variable answer modify by you
-    private int Function = 0;
-    private int FindAdressWord04Result = 0;
+    private string correctAns = ""; //Answer correct.
+    private string yourAns = ""; //Variable answer modify by you
+
     // Use this for initialization
-    void Start ()
+    private void Start()
     {
         _moduleId = _moduleIdCounter++;
         Module.OnActivate += Activate;
-	}
-	
-    void Activate()
+    }
+
+    private void Activate()
     {
         Init();
         _lightsOn = true;
     }
 
-    void Init()
+    private void Init()
     {
         Debug.LogFormat("[ ModBus #{0} ]", _moduleId);
 
-        //Init the variable.
-        digit = 0;
-        InitTab();
-        FindCorrectAns();
-        UpdateScreenText();
+        // Find correct answer
+        Find_Correct_Answer();
     }
 
-    private void FindCorrectAns ()
+    private void Find_Correct_Answer()
     {
-        FindSlaveAdress();
-        FindFunction();
-        Function = 06;
-        if(Function == 04)
-        {
-            FindAdressWord04();
-            FindNumberOfWord04();
+        // Slave ID
+        Find_Correct_SlaveId();
+
+        // Function
+        Find_Correct_Function();
+
+        // If function 04
+        if(correctAns[3] == '4'){
+            Debug.LogFormat("[ ModBus #{0} - Find_Correct_Answer - function 4 detected]", _moduleId);
+
+            // Address function 04
+            Find_Correct_Function_04_Address();
+
+            // Number of word to read function 04
+            Find_Correct_Function_04_Number_of_Word();
+
+        } else {  // If function 06
+            Debug.LogFormat("[ ModBus #{0} - Find_Correct_Answer - function 6 detected]", _moduleId);
+
+            // Address function 06
+            Find_Correct_Function_06_Address();
+
+            // Value function 06
+            Find_Correct_Function_06_Value();
         }
-        else if(Function == 06)
-        {
-            FindAdressInRegister06();
-            FindValue06();
-        }
-        else
-        {
-            Debug.LogFormat("[ ModBus #{0} Erreur Function N°{1}]", _moduleId, Function);
-        }
+
     }
 
-    private void UpdateScreenText ()
+    // For SlaveID :
+    // - Convert 1st number/letter from ASCII to hexadecimal
+    private void Find_Correct_SlaveId()
     {
-        Screen.text = new string(YourAns);
-        //Conv all char in a string for the screentext
+        // Convert 1st digit to decimal
+        int Digit1 = Info.GetSerialNumber().ElementAt(0);
+
+        // Convert decimal to string hexa
+        string slaveId = Digit1.ToString("X");
+
+        correctAns = slaveId;
+        Debug.LogFormat("[ ModBus #{0} - Find_Correct_SlaveId - first digit : {1} - slaveId : {2}]", _moduleId, Digit1, slaveId);
+    }
+
+    // For Function :
+    // - Convert 6th digit from ASCII to decimal
+    // - Take the rest of modulo 3
+    // - Multiply by 7
+    // - Take modulo 2
+    // - if 0 -> 06, if 1 -> 04
+    private void Find_Correct_Function()
+    {
+        // Convert 6th digit from ASCII to decimal
+        int Digit6 = Info.GetSerialNumber().ElementAt(5);
+        string function = "";
+
+        // Take modulo 3
+        int temp = Digit6 % 3;
+
+        // Multiply by 7
+        temp *= 7;
+
+        // Take modulo 2
+        temp %= 3;
+
+        // if 0 -> 06, if 1 -> 04
+        if(temp == 0){
+            function = "06";
+        } else {
+            function = "04";
+        }
+        correctAns += function;
+        Debug.LogFormat("[ ModBus #{0} - Find_Correct_Function - 6th digit : {1} - function : {2}]", _moduleId, Digit6, function);
+    }
+
+    // For Function 04 - address:
+    // - take 3rd digit
+    // - if it's a letter, convert it from ASCII to decimal
+    // - take 4th digit
+    // - if it's a letter, convert it from ASCII to decimal
+    // - Multiply the 2 previous number
+    // - Convert to hexa
+    private void Find_Correct_Function_04_Address()
+    {
+        // take 3rd digit
+        int Digit3 = Info.GetSerialNumber().ElementAt(2);
+
+        // if it's a number, the conversion from ascii is not automatic
+        if(Digit3 >= '0' && Digit3 <= '9')
+        {
+            // Soustract by ASCII code of 0, 48
+            Digit3 -= 48;
+        }
+
+        // take 4th digit
+        int Digit4 = Info.GetSerialNumber().ElementAt(3);
+
+        // if it's a number, the conversion from ascii is not automatic
+        if (Digit4 >= '0' && Digit4 <= '9')
+        {
+            // Soustract by ASCII code of 0, 48
+            Digit4 -= 48;
+        }
+
+        // Multiply the 2 previous number
+        int Result_Mult = Digit3 * Digit4;
+
+        // Convert to hexa
+        string address = Result_Mult.ToString("X");
+
+        correctAns += address;
+        Debug.LogFormat("[ ModBus #{0} - Find_Correct_Function_04_Address - digit 3 : {1} - digit 4 : {2} - address : {3}]", _moduleId, Digit3, Digit4, address);
+
+    }
+
+    // For Function 04 - number of word:
+    // - take 5th digit
+    // - Convert to ASCII to binary
+    // - Shift 6 times to the left, it's new 5th digit
+    // - take 6th digit
+    // - Convert to ASCII to binary
+    // - Make OR Bitwise with new 5th and 6th
+    private void Find_Correct_Function_04_Number_of_Word()
+    {
+        // take 5th digit
+        int Digit5 = Info.GetSerialNumber().ElementAt(4);
+
+        // Shift 6 times to the left, it's new 5th digit
+        Digit5 <<= 6;
+
+        // take 6th digit
+        int Digit6 = Info.GetSerialNumber().ElementAt(5);
+
+        // Make OR Bitwise with new 5th and 6th
+        int NumberOfWord = Digit5 & Digit6;
+
+        correctAns += NumberOfWord;
+        Debug.LogFormat("[ ModBus #{0} - Find_Correct_Function_04_Number_of_Word - digit 5 : {1} - digit 6 : {2} - number of word : {3}]", _moduleId, Digit5, Digit6, NumberOfWord);
+    }
+
+    // For Function 06 - address:
+    // - Take 4th digit
+    // - Convert from ASCII to binary
+    // - Make not in 16 bits
+    // - Convert to hexa
+    private void Find_Correct_Function_06_Address()
+    {
+        // Take 4th digit
+        int Digit4 = Info.GetSerialNumber().ElementAt(3);
+
+        // Make not in 16 bits
+        int Result_Not = Digit4 ^ 0xFFFF;
+
+        // Convert to hexa
+        string address = Result_Not.ToString("X");
+
+        correctAns += address;
+        Debug.LogFormat("[ ModBus #{0} - Find_Correct_Function_06_Address - digit 4 : {1} - number of word : {2}]", _moduleId, Digit4, address);
+    }
+
+    // For Function 06 - value:
+    // - Add every number together
+    // - Convert it to hexadecimal
+    // - troncate if necessary
+    private void Find_Correct_Function_06_Value()
+    {
+        // Make the sum of all number
+        int sum = 0;
+        int actualSerialDigit; 
+        // For all serial number
+        for(int i=0; i<6; i++)
+        {
+            actualSerialDigit = Info.GetSerialNumber().ElementAt(i);
+            // if it's a number
+            if (actualSerialDigit >= '0' && actualSerialDigit <= '9')
+            {
+                // Soustract by ASCII code of 0, 48
+                actualSerialDigit -= 48;
+                sum += actualSerialDigit;
+            }
+        }
+
+        // Convert it to hexadecimal
+        string value = sum.ToString("X");
+        correctAns += value;
+
+        Debug.LogFormat("[ ModBus #{0} - Find_Correct_Function_06_Value - value : {1}]", _moduleId, value);
+    }
+    /*
+    // UpdateScreenText :
+    // - Update screen text with yourAns
+    private void UpdateScreenText()
+    {
+        Screen.text = yourAns;
     }
 
     private void Awake()
@@ -109,7 +274,7 @@ public class ModBusScript : MonoBehaviour {
     {
         if (!_lightsOn || _isSolved) return;
 
-        if (CorrectAns.SequenceEqual(YourAns))
+        if (correctAns == yourAns)
         {
             //You win
             GetComponent<KMBombModule>().HandlePass();
@@ -125,167 +290,30 @@ public class ModBusScript : MonoBehaviour {
     {
         if (!_lightsOn || _isSolved) return;
 
-        YourAns[digit] = IntToASCII(num); //Need to convert, NumToASCII(num) -> ASCII
-        Debug.LogFormat("[ ModBus #{0} - AnsDigit N°{1} -> {2} ]", _moduleId, digit, YourAns[digit]);
+        yourAns += num;
+        Debug.LogFormat("[ ModBus #{0} - HandlePress - num : {2} ]", _moduleId, num=);
         UpdateScreenText();
 
-        if (digit % 3 == 1) // If is a '.' digit (for display)
+        if (yourAns.Length % 3 == 1) // If is a '.' digit (for display)
         {
             digit++;
         }
 
         if (digit >= 16) //Test if you are at the last digit
         {
-            digit = 0; 
+            digit = 0;
         }
         else
         {
-            digit++; 
+            digit++;
         }
-    }
-
-
-    private char IntToASCII(int num)
-    {
-        //Debug.LogFormat("IntToASCII IN -> {0}", num);
-        if (num<=9) //If is an number, convert to ASCII number
-        {
-            num = num + 48;
-        }
-        else
-        {
-            num = num + 55; //A = 10 so 10 + 55 = 65 -> 'A'
-        }
-        //Debug.LogFormat("IntToASCII OUT -> {0}", (char)num);
-        return (char)num;
-    }
-
-    private void InitTab()
-    {
-        for(int i=0; i<17; i++)
-        {
-            if((i%3 == 0)||(i%3==1))
-            {
-                CorrectAns[i] = '_';
-                YourAns[i] = '_';
-            }
-            else
-            {
-                CorrectAns[i] = '.';
-                YourAns[i] = '.';
-            }
-        }
-    }
-
-    private void FindSlaveAdress()
-    {
-        
-        string SlaveAdress = BitConverter.ToString(new byte[] { Convert.ToByte(Info.GetSerialNumber().ElementAt(0)) });
-        CorrectAns[0] = SlaveAdress.ToCharArray()[0];
-        CorrectAns[1] = SlaveAdress.ToCharArray()[1];
-        //Debug.LogFormat("[ ModBus #{0} - Slave Adress {1}]", _moduleId, SlaveAdress);
-        Debug.LogFormat("[ ModBus #{0} - Partiel Correct Answer {1}]", _moduleId, new string(CorrectAns));
-    }
-
-    private void FindFunction()
-    {
-        Function = Info.GetSerialNumber().ElementAt(5) % 2;
-        Function = Function * 2 + 4;
-        CorrectAns[3] = '0';
-        CorrectAns[4] = (char)(Function + 48);
-        Debug.LogFormat("[ ModBus #{0} - Partiel Correct Answer {1}]", _moduleId, new string(CorrectAns));
-    }
-
-    private void FindAdressWord04()
-    {
-        int firstNumber = Info.GetSerialNumber().ElementAt(2);
-        int secondNumber = Info.GetSerialNumber().ElementAt(3);
-        if(firstNumber<58) //if is a number
-        {
-            firstNumber -= 48;
-        }
-        if (secondNumber < 58) //if is a number
-        {
-            firstNumber -= 48;
-        }
-
-        FindAdressWord04Result = firstNumber * secondNumber * 100;
-
-        string SlaveAdress = BitConverter.ToString(new byte[] { Convert.ToByte(FindAdressWord04Result / 256)});
-        string SlaveAdress2 = BitConverter.ToString(new byte[] { Convert.ToByte(FindAdressWord04Result % 256)});
-        CorrectAns[6] = SlaveAdress.ToCharArray()[0];
-        CorrectAns[7] = SlaveAdress.ToCharArray()[1];
-
-        CorrectAns[9] = SlaveAdress2.ToCharArray()[0];
-        CorrectAns[10] = SlaveAdress2.ToCharArray()[1];
-
-        Debug.LogFormat("[ ModBus #{0} - Partiel Correct Answer {1}]", _moduleId, new string(CorrectAns));
-    }
-
-    private void FindNumberOfWord04()
-    {
-        int FindNumberOfWord04result;
-        FindNumberOfWord04result = Info.GetSerialNumber().ElementAt(0) * Function + Info.GetSerialNumber().ElementAt(4);
-
-        string SlaveAdress = BitConverter.ToString(new byte[] { Convert.ToByte(FindNumberOfWord04result / 256) });
-        string SlaveAdress2 = BitConverter.ToString(new byte[] { Convert.ToByte(FindNumberOfWord04result % 256) });
-
-        CorrectAns[12] = SlaveAdress.ToCharArray()[0];
-        CorrectAns[13] = SlaveAdress.ToCharArray()[1];
-
-        CorrectAns[15] = SlaveAdress2.ToCharArray()[0];
-        CorrectAns[16] = SlaveAdress2.ToCharArray()[1];
-
-        Debug.LogFormat("[ ModBus #{0} - Partiel Correct Answer {1}]", _moduleId, new string(CorrectAns));
-    }
-
-    private void FindAdressInRegister06()
-    {
-        byte firstRegister = 0x00; 
-        byte secondRegister = 0x00;
-        byte[] temp;
-
-        temp = new byte[] { Convert.ToByte(Info.GetSerialNumber().ElementAt(3)) };
-        firstRegister = temp[0];
-
-        Debug.LogFormat("[ ModBus #{0} - firstRegister -> {1}]", _moduleId, firstRegister);
-
-        secondRegister = (byte)(firstRegister ^ 0xFF);
-        secondRegister = (byte)(secondRegister % 100);
-
-        Debug.LogFormat("[ ModBus #{0} - secondeRegister -> {1}]", _moduleId, secondRegister);
-
-        CorrectAns[6] = (char)(firstRegister/10+48);
-        CorrectAns[7] = (char)(firstRegister % 10 + 48);
-
-        CorrectAns[9] = (char)(secondRegister / 10 + 48);
-        CorrectAns[10] = (char)(secondRegister % 10 + 48);
-
-        Debug.LogFormat("[ ModBus #{0} - Partiel Correct Answer {1}]", _moduleId, new string(CorrectAns));
-    }
-
-    private void FindValue06()
-    {
-        int result;
-        result = (Info.GetSerialNumber().ElementAt(0) - Function) * Info.GetSerialNumber().ElementAt(4); 
-
-        Debug.LogFormat("[ ModBus #{0} - Test {1}]", _moduleId, result);
-
-        CorrectAns[12] = (char)(result % 10000 /1000 + 48);
-        CorrectAns[13] = (char)(result % 1000 / 100 + 48);
-
-        CorrectAns[15] = (char)(result % 100 / 10 + 48);
-        CorrectAns[16] = (char)(result % 10 + 48);
-
-
-        Debug.LogFormat("[ ModBus #{0} - Partiel Correct Answer {1}]", _moduleId, new string(CorrectAns));
-    }
+    }*/
 }
 
-/*USEFULL FUNCTION
- * Info.GetSerialNumber().ElementAt(0); //Get number from serialNumber. (return char)
- * (char)122 Convert like a savage an int in char
- * 
- * 
- * 
- * */
+
+
+
+
+
+
+
